88091039b5358f739a3d591d8a22648ca2be6649
995 <1423747022@qq.com>
2025-03-09 15:08:22 +0800

    '菜单and头部'

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,12 +2,11 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import Login from '../views/login/index.vue'
 import Menu from '../views/menu/index.vue'
 import Home from '../views/home/index.vue'
-import component from 'element-plus/es/components/tree-select/src/tree-select-option.mjs'
 // 创建路由
 const routes = [
     {
         path: "/",
-        redirect: "login"
+        redirect: "menu"
     },
     {
         name: "login",
